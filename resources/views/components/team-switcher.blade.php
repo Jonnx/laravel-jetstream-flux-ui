792907164c58d5a7d83fdf89a1c9d0eb5a7c4437
Jonas Weigert <weigert.jonas@gmail.com>
2025-11-24 10:48:30 -0600

    wip

---
+++ resources/views/components/team-switcher.blade.php
@@ -0,0 +1,30 @@
+<flux:dropdown position="top" align="start" offset="3">
+    <flux:profile
+        :initials="initials(auth()->user()->currentTeam->name)"
+        :name="auth()->user()->currentTeam->name"
+        icon-trailing="chevron-down"
+    />
+    <flux:menu>
+        {{-- CURRENT TEAM SHORTCUTS  --}}
+        <flux:menu.group>
+            <flux:menu.item :href="route('teams.show', ['team' => auth()->user()->currentTeam->id])" icon="cog" wire:navigate>{{ __('Team Settings') }}</flux:menu.item>
+        </flux:menu.group>
+
+        {{-- USER'S AVAILABLE TEAMS --}}
+        <flux:menu.group heading="{{ __('My Teams') }}">
+
+            {{-- SWITCHABLE TEAMS --}}
+            @foreach (auth()->user()->allTeams() as $userTeam)
+            <x-switchable-team :team="$userTeam" />
+            @endforeach
+
+
+
+        </flux:menu.group>
+
+        <flux:menu.group>
+                {{-- ADD A TEAM --}}
+            <flux:menu.item :href="route('teams.create')" icon="plus" wire:navigate>{{ __('Create a new Team') }}</flux:menu.item>
+        </flux:menu.radio.group>
+    </flux:menu>
+</flux:dropdown>
