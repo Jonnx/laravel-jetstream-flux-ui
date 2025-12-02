23fdb602f60c7a2430280b87973a74bdf1435909
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-02 11:36:33 -0600

    wip

---
+++ resources/views/components/modal.blade.php
@@ -0,0 +1,21 @@
+@props(['id', 'maxWidth'])
+
+@php
+$id = $id ?? md5($attributes->wire('model'));
+
+$maxWidth = [
+    'sm' => 'sm:max-w-sm',
+    'md' => 'sm:max-w-md',
+    'lg' => 'sm:max-w-lg',
+    'xl' => 'sm:max-w-xl',
+    '2xl' => 'sm:max-w-2xl',
+][$maxWidth ?? '2xl'];
+@endphp
+
+<flux:modal
+    id="{{ $id }}"
+    class="w-full {{ $maxWidth }}"
+    {{ $attributes }}
+>
+    {{ $slot }}
+</flux:modal>
