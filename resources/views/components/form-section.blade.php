1069acec9bd0beb899e6c8eccc4f2122c967c77e
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-02 10:17:59 -0600

    wip

--- resources/views/components/form-section.blade.php
+++ resources/views/components/form-section.blade.php
@@ -1,23 +1,8 @@
 @props(['submit' => null])
 
 <section {{ $attributes }}>
-    <div class="mt-5 md:mt-0 md:col-span-2">
-        @if($submit)
-            <form wire:submit="{{ $submit }}">
-                <flux:card class="p-6 overflow-hidden">
-                    <flux:heading>{{ $title }}</flux:heading>
-                    <flux:text class="mt-2 mb-6">{{ $description }}</flux:text>
-                    <div class="grid grid-cols-6 gap-6">
-                        {{ $form }}
-                    </div>
-                    @if (isset($actions))
-                        <div class="flex items-center justify-end p-6 mt-6 -m-6 bg-neutral-100 dark:bg-neutral-700">
-                            {{ $actions }}
-                        </div>
-                    @endif
-                </flux:card>
-            </form>
-        @else
+    @if($submit)
+        <form wire:submit="{{ $submit }}">
             <flux:card class="p-6 overflow-hidden">
                 <flux:heading>{{ $title }}</flux:heading>
                 <flux:text class="mt-2 mb-6">{{ $description }}</flux:text>
@@ -25,11 +10,24 @@
                     {{ $form }}
                 </div>
                 @if (isset($actions))
-                    <div class="flex items-center justify-end p-6 mt-6 -m-6 bg-neutral-100 dark:bg-neutral-70">
+                    <div class="flex items-center justify-end p-6 mt-6 -m-6 bg-neutral-100 dark:bg-neutral-700">
                         {{ $actions }}
                     </div>
                 @endif
             </flux:card>
-        @endif
-    </div>
+        </form>
+    @else
+        <flux:card class="p-6 overflow-hidden">
+            <flux:heading>{{ $title }}</flux:heading>
+            <flux:text class="mt-2 mb-6">{{ $description }}</flux:text>
+            <div class="grid grid-cols-6 gap-6">
+                {{ $form }}
+            </div>
+            @if (isset($actions))
+                <div class="flex items-center justify-end p-6 mt-6 -m-6 bg-neutral-100 dark:bg-neutral-70">
+                    {{ $actions }}
+                </div>
+            @endif
+        </flux:card>
+    @endif
 </section>
---
+++ resources/views/components/section-border.blade.php
@@ -0,0 +1,3 @@
+<div class="hidden sm:block">
+    <div class="py-6"></div>
+</div>
