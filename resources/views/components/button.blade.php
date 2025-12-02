c0dbfe6f26dc7a3eebc9c9e64913aa52e1a4ffce
Jonas Weigert <weigert.jonas@gmail.com>
2025-12-02 11:05:54 -0600

    wip

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -1,3 +1,3 @@
-<flux:button variant="primary" {{ $attributes->merge(['type' => 'submit']) }}>
+<flux:button variant="primary" {{ $attributes->merge(['type' => 'submit', 'class' => 'cursor-pointer']) }}>
     {{ $slot }}
 </flux:button>
--- resources/views/components/secondary-button.blade.php
+++ resources/views/components/secondary-button.blade.php
@@ -1,3 +1,3 @@
-<flux:button variant="subtle" {{ $attributes->merge(['type' => 'button']) }}>
+<flux:button variant="subtle" {{ $attributes->merge(['type' => 'button', 'class' => 'cursor-pointer']) }}>
     {{ $slot }}
 </flux:button>
